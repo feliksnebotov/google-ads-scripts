de2cf971c6cd8a0f943275836337bde5e3710342
feliksn <feliksn@outlook.com>
2017-11-20 22:15:05 +0300

    Удалил лишний код

--- cleanerGdnCpa.js
+++ cleanerGdnCpa.js
@@ -1,4 +1,4 @@
-var EXCLUDED_PLACEMENT_LIST_NAME = "CleanerRemarketing";
+var EXCLUDED_PLACEMENT_LIST_NAME = "CleanerRemarketing"; //Название списка минус-площадок в библиотеке. Если список не создан, скрипт сам создаст список
 
 function main() {
 
@@ -8,15 +8,12 @@ function main() {
     var lowCtr = 0.05; ////Площадки с CTR меньше lowCtr отключаются
     var lowImpressions = 2000; //при числе показов не менее lowImpressions 
     var clicksLimit = 100; //Предельное число кликов. Если кликов больше этого числа и нет конверсий, то площадка исключается
-    //var ignoreAnonymous = 1; //Игнорировать anonymous.google, 0 будет исключать
-    //var ignorePlacements = 1; //Игнорировать список ваших площадок, 0 будет исключать
     var exclude = ['avito.ru', 'youtube.com']; //Список площадок для исключения
     var limitCpa = 8000; //Исключаем по стоимости CPA
     var during = "LAST_30_DAYS"; //Период сбора данных
 
     //TODAY | YESTERDAY | LAST_7_DAYS | THIS_WEEK_SUN_TODAY | THIS_WEEK_MON_TODAY | LAST_WEEK |
     //LAST_14_DAYS | LAST_30_DAYS | LAST_BUSINESS_WEEK | LAST_WEEK_SUN_SAT | THIS_MONTH
-
 
     var campaignArray = [];
     var excludePlacementArray = [];
@@ -51,16 +48,13 @@ function main() {
         costPerConversion = parseInt(costPerConversion.replace(",", ""));
         cost = parseInt(cost.replace(",", ""));
 
-        // if (ignoreAnonymous && domain.toString()=="anonymous.google") continue;
-        // if (ignorePlacements && domain.toString()=="avito.ru" || domain.toString()=="youtube.com") continue;
-
         if (domain.indexOf("mobileapp") == 0) domain = domain.replace(/mobileapp::\d+-/, "") + ".adsenseformobileapps.com";
 
         if (containsAny(domain.toString(), exclude)) {
             excludePlacementArray[excludePlacementArray.length] == domain.toString();
             continue;
         }
-        
+
         //HYPE
         if ((clicks / impressions * 100 >= hypeCtr) && (impressions >= hypeImpressions) && (сonversions < 1)) {
             excludePlacementArray.push(domain.toString());
@@ -79,7 +73,7 @@ function main() {
         //2xCPA
         if ((cost > limitCpa) && (сonversions == 0) || (costPerConversion > limitCpa)) {
             excludePlacementArray.push(domain.toString());
-          Logger.log('Excluded (Cost per conversion limit): Conversions: ' + сonversions + ', Cost: ' + cost + ', CPA: ' + costPerConversion + ', ' + domain.toString());
+            Logger.log('Excluded (Cost per conversion limit): Conversions: ' + сonversions + ', Cost: ' + cost + ', CPA: ' + costPerConversion + ', ' + domain.toString());
         }
     }
 
